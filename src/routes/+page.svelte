9d22c5796917078974a8decfe204fd2a9d30eabd
FS-Frost <30810344+FS-Frost@users.noreply.github.com>
2024-05-12 13:36:21 -0400

    Router.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,27 +1,9 @@
 <script lang="ts">
-    import { activePage, type ActivePage } from "$lib/store";
-    import { onMount } from "svelte";
-    import CopiarMovimiento from "$lib/pages/carteles/CopiarMovimiento.svelte";
-    import AcercaDe from "$lib/pages/AcercaDe.svelte";
-    import Inicio from "$lib/pages/Inicio.svelte";
-
-    let page: ActivePage = "inicio";
-
-    onMount(() => {
-        activePage.subscribe((newActivePage) => {
-            page = newActivePage;
-        });
-    });
+    import Router from "./Router.svelte";
 </script>
 
 <section>
-    {#if page == "inicio"}
-        <Inicio />
-    {:else if page == "carteles.copiarMovimiento"}
-        <CopiarMovimiento />
-    {:else if page == "acercaDe"}
-        <AcercaDe />
-    {/if}
+    <Router />
 </section>
 
 <style>
---
+++ src/routes/Router.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+    import { activePage, type ActivePage } from "$lib/store";
+    import { onMount } from "svelte";
+    import CopiarMovimiento from "$lib/pages/carteles/CopiarMovimiento.svelte";
+    import AcercaDe from "$lib/pages/AcercaDe.svelte";
+    import Inicio from "$lib/pages/Inicio.svelte";
+
+    let page: ActivePage = "inicio";
+
+    onMount(() => {
+        activePage.subscribe((newActivePage) => {
+            page = newActivePage;
+        });
+    });
+</script>
+
+{#if page == "inicio"}
+    <Inicio />
+{:else if page == "carteles.copiarMovimiento"}
+    <CopiarMovimiento />
+{:else if page == "acercaDe"}
+    <AcercaDe />
+{/if}
