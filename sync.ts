fe4cd2177420997567ea5b0a880d1403b5eb9d46
FS-Frost <30810344+FS-Frost@users.noreply.github.com>
2025-10-23 20:42:20 -0300

    Ajustes.

--- sync.ts
+++
@@ -1,5 +0,0 @@
-import { $ } from "bun";
-
-await $`cp ../asu-regex-js/build/asu.browser.js src/lib/asu/asu.browser.js`;
-await $`cp ../asu-regex-js/build/asu.browser.js.map src/lib/asu/asu.browser.js.map`;
-await $`cp ../asu-regex-js/build/asu.browser.d.ts src/lib/asu/asu.browser.d.ts`;
